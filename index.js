ac2e4b6c250fcc83064b5e3b90640682dc95a5ea
younuskhan1 <md.younuskhan412@gmail.com>
2023-11-08 10:21:32 +0600

    navigation to bank file is completed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("bank.html")
+function getFormInputValue(inputId) {
+    const formField = document.getElementById(inputId);
+    const formInputValue = formField.value;
+    formField.value = "";
+    return formInputValue;
+}
+
+document.getElementById("button-submit").addEventListener("click", function () {
+    const userEmail = getFormInputValue("user-email");
+    const userPassword = getFormInputValue("user-password");
+    if ((userEmail === "irfanjamil.du93rd@gmail.com") && (userPassword === "amarsonarbangla")) {
+        window.location.href = "bank.html";
+    } else {
+        alert("You are an invalid user. Please enter the correct email and password.");
+    }
+})
